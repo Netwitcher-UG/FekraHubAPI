518fe009ce135a4046470e280fc4acbb76337e5f
basel.slaby@gmail.com <basel.slaby@gmail.com>
2024-05-26 16:49:50 +0300

    attendance / attendance status

---
+++ FekraHubAPI/Models/Courses/mdl_StudentAttendance.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FekraHubAPI.Data.Models;
+namespace FekraHubAPI.Models.Courses
+{
+    public class Mdl_StudentAttendance : IMapFrom<StudentAttendance>
+    {
+        public DateTime Date { get; set; }
+        public int? CourseID { get; set; }
+        public int? StudentID { get; set; }
+        public int? StatusID { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Mdl_StudentAttendance, StudentAttendance>();
+        }
+    }
+}
--- FekraHubAPI/Repositories/Implementations/GenericRepository.cs
+++ FekraHubAPI/Repositories/Implementations/GenericRepository.cs
@@ -1,6 +1,7 @@
 using FekraHubAPI.Data;
 using FekraHubAPI.Data.Models;
 using FekraHubAPI.Repositories.Interfaces;
+using FekraHubAPI.Seeds;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -50,9 +51,33 @@ namespace FekraHubAPI.Repositories.Implementations
                 await _context.SaveChangesAsync();
             }
         }
-
-      
-
+        public async Task<IQueryable<T>> GetRelation()
+        {
+            return await Task.FromResult(_dbSet.AsQueryable());
+        }
+        public async Task ManyAdd(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+        public async Task SaveManyAdd()
+        {
+            await _context.SaveChangesAsync();
+        }
+        public async Task<bool> IDExists(int id)
+        {
+            return await _dbSet.FindAsync(id) != null;
+        }
+        public async Task<bool> IsTeacherIDExists(string userId)
+        {
+            var roles = await _context.Roles.FirstOrDefaultAsync(r => r.Name == RolesEnum.Teacher.ToString());
+            if (roles == null)
+            {
+                return false;
+            }
+            var user = await _context.Users.FindAsync(userId);
+            var userRoles = await _context.UserRoles.FirstOrDefaultAsync(x => x.UserId == userId && x.RoleId == roles.Id);
+            return user != null && userRoles != null;
+        }
 
 
     }

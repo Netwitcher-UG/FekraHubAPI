b16340be787f6f75903d722614089e0be481bde7
walaMousa <walaaalmousa68@gmail.com>
2024-05-30 02:09:45 +0300

    schedule

--- FekraHubAPI/Migrations/20240528184056_timeSchedule.Designer.cs
+++ FekraHubAPI/Migrations/20240528184056_timeSchedule.Designer.cs
@@ -4,6 +4,7 @@ using FekraHubAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FekraHubAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240528184056_timeSchedule")]
+    partial class timeSchedule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -121,10 +124,10 @@ namespace FekraHubAPI.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<TimeSpan>("EndTime")
+                    b.Property<TimeSpan>("EndDate")
                         .HasColumnType("time");
 
-                    b.Property<TimeSpan>("StartTime")
+                    b.Property<TimeSpan>("StartDate")
                         .HasColumnType("time");
 
                     b.HasKey("Id");
@@ -218,9 +221,8 @@ namespace FekraHubAPI.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                    b.Property<byte[]>("File")
-                        .IsRequired()
-                        .HasColumnType("varbinary(max)");
+                    b.Property<byte>("File")
+                        .HasColumnType("tinyint");
 
                     b.Property<int?>("StudentID")
                         .HasColumnType("int");
@@ -243,9 +245,8 @@ namespace FekraHubAPI.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                    b.Property<byte[]>("File")
-                        .IsRequired()
-                        .HasColumnType("varbinary(max)");
+                    b.Property<byte>("File")
+                        .HasColumnType("tinyint");
 
                     b.Property<DateTime>("Timestamp")
                         .HasColumnType("datetime2");
@@ -402,9 +403,8 @@ namespace FekraHubAPI.Migrations
                     b.Property<DateTime>("CreationDate")
                         .HasColumnType("datetime2");
 
-                    b.Property<byte[]>("File")
-                        .IsRequired()
-                        .HasColumnType("varbinary(max)");
+                    b.Property<byte>("File")
+                        .HasColumnType("tinyint");
 
                     b.Property<int?>("StudentID")
                         .HasColumnType("int");
@@ -540,9 +540,8 @@ namespace FekraHubAPI.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
-                    b.Property<byte[]>("File")
-                        .IsRequired()
-                        .HasColumnType("varbinary(max)");
+                    b.Property<byte>("File")
+                        .HasColumnType("tinyint");
 
                     b.Property<string>("TeacherID")
                         .HasColumnType("nvarchar(450)");
@@ -891,7 +890,7 @@ namespace FekraHubAPI.Migrations
                     b.HasData(
                         new
                         {
-                            UserId = "8f18d45c-89b7-425c-b40e-78cfac4bdd2b",
+                            UserId = "1749db01-d187-4db4-b0ea-9b0841ec77a6",
                             RoleId = "1"
                         });
                 });
@@ -986,18 +985,18 @@ namespace FekraHubAPI.Migrations
                     b.HasData(
                         new
                         {
-                            Id = "8f18d45c-89b7-425c-b40e-78cfac4bdd2b",
+                            Id = "1749db01-d187-4db4-b0ea-9b0841ec77a6",
                             AccessFailedCount = 0,
-                            ConcurrencyStamp = "a81f9f4f-f74a-4162-a519-63b3ff78b854",
+                            ConcurrencyStamp = "2151edce-67e9-48b4-8a77-0be60a351dca",
                             Email = "admin@admin.com",
                             EmailConfirmed = false,
                             LockoutEnabled = false,
                             NormalizedEmail = "admin@admin.com",
                             NormalizedUserName = "ADMIN",
-                            PasswordHash = "AQAAAAIAAYagAAAAEEQmGin2jc+eRmWhy7rQmvh3PUcEVDn97lMVShjJ8/4BqnYIX9rMuCczeJD4iFFzjw==",
+                            PasswordHash = "AQAAAAIAAYagAAAAELVKafh5bVgceNWLEITUxQyWjTQTauC5c1RS8V0aZ87AoovksWdvWgsHYVmIQmW6Qw==",
                             PhoneNumber = "+1234567890",
                             PhoneNumberConfirmed = false,
-                            SecurityStamp = "505a3c4f-2a76-405d-8557-e49824134c9b",
+                            SecurityStamp = "5be71d22-4be2-42f6-a2eb-240f65d56dc6",
                             TwoFactorEnabled = false,
                             UserName = "Admin",
                             Birthday = new DateTime(1985, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),

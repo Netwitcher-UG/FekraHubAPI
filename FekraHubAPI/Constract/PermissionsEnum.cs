620a166c142dc70c478f40523ebf0523138cf90d
hala nabeel <halanabeel76@gmail.com>
2024-06-04 12:50:39 +0300

    update permissions name

--- FekraHubAPI/Constract/PermissionsEnum.cs
+++ FekraHubAPI/Constract/PermissionsEnum.cs
@@ -10,54 +10,52 @@ namespace FekraHubAPI.Constract
 
         public enum AllPermissions
         {
-            View_Users,
-            Add_Users,
-            View_Student_Placement,
-            Edit_Student_Placement,
-            View_Teacher_Placement,
-            Edit_Teacher_Placement,
-            View_Contracts,
-            Manage_Contracts,
-            View_Events,
-            View_General_Attendance,
-            Edit_General_Attendance,
-            View_Teacher_Attendance,
-            Edit_Teacher_Attendance,
-            View_Reports,
-            Approve_Reports,
-            Add_Bills,
-            View_Bills,
-            View_Books,
-            Delete_Book,
-            Delete_School_Aata,
-            Reset_Data
+            GetUsers
+            AddUsers
+            GetStudentsCourse
+            UpdateStudentsCourse
+            GetTeachersCourse
+            UpdateTeachersCourse
+            GetContracts
+            ManageEvents
+            GetEvents
+            GetStudentsAttendance
+            UpdateStudentsAttendance
+            GetTeachersAttendance
+            UpdateTeachersAttendance
+            InsertUpdateStudentsReports
+            GetStudentsReports
+            ApproveReports
+            ManagePayrolls
+            ManageBooks
+            DeleteSchoolData
+            ResetData
 
 
         }
 
         public enum PermissionModuleNameAdmin
         {
-            View_Users,
-            Add_Users,
-            View_Student_Placement,
-            Edit_Student_Placement,
-            View_Teacher_Placement,
-            Edit_Teacher_Placement,
-            View_Contracts,
-            Manage_Contracts,
-            View_Events,
-            View_General_Attendance,
-            Edit_General_Attendance,
-            View_Teacher_Attendance,
-            Edit_Teacher_Attendance,
-            View_Reports,
-            Approve_Reports,
-            Add_Bills,
-            View_Bills,
-            View_Books,
-            Delete_Book,
-            Delete_School_Aata,
-            Reset_Data
+            GetUsers
+            AddUsers
+            GetStudentsCourse
+            UpdateStudentsCourse
+            GetTeachersCourse
+            UpdateTeachersCourse
+            GetContracts
+            ManageEvents
+            GetEvents
+            GetStudentsAttendance
+            UpdateStudentsAttendance
+            GetTeachersAttendance
+            UpdateTeachersAttendance
+            InsertUpdateStudentsReports
+            GetStudentsReports
+            ApproveReports
+            ManagePayrolls
+            ManageBooks
+            DeleteSchoolData
+            ResetData
 
         }
 

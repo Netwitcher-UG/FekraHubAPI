8bbe9854dad7d5701abba754736528dc0912beb2
hala nabeel <halanabeel76@gmail.com>
2024-06-04 12:39:33 +0300

    permissions managment

--- FekraHubAPI/Controllers/Helper.cs
+++ FekraHubAPI/Controllers/Helper.cs
@@ -2,19 +2,8 @@
 {
     public class Helper
     {
-        public const string Permission = "Permission";
-        public enum PermissionModuleNameAdmin {
-            Create_User,
-            View_User,
 
-        }
 
-        public  enum AllRolesE
-        {
-            Admin,
-            Secretariat,
-            Parent,
-            Teacher,
-        }
+
     }
 }

73a5e6b212e5d1c6f8d205c24be4f78d4703da9f
sulaf-93 <sulaf-93>
2024-06-10 11:42:33 +0300

    form children

--- FekraHubWeb/src/views/pages/parents/children/RegisterChildren.jsx
+++ FekraHubWeb/src/views/pages/parents/children/RegisterChildren.jsx
@@ -10,17 +10,13 @@ import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import Select from '@mui/material/Select'
-import Radio from '@mui/material/Radio'
-import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Button from '@mui/material/Button'
 import Checkbox from '@mui/material/Checkbox'
-import FormLabel from '@mui/material/FormLabel'
 import FormHelperText from '@mui/material/FormHelperText'
 import MenuItem from '@mui/material/MenuItem'
 import InputLabel from '@mui/material/InputLabel'
 import FormControl from '@mui/material/FormControl'
-import InputAdornment from '@mui/material/InputAdornment'
 import IconButton from '@mui/material/IconButton'
 
 // Third-party Imports
@@ -30,7 +26,7 @@ import { useForm, Controller } from 'react-hook-form'
 // Styled Component Imports
 import AppReactDatepicker from '@/libs/styles/AppReactDatepicker'
 
-const FormValidationBasic = () => {
+const RegisterChildren = () => {
   // States
   const [isPasswordShown, setIsPasswordShown] = useState(false)
 
@@ -59,7 +55,7 @@ const FormValidationBasic = () => {
 
   return (
     <Card>
-      <CardHeader title='Basic' />
+      <CardHeader title='Create Student' />
       <CardContent>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Grid container spacing={5}>
@@ -81,7 +77,7 @@ const FormValidationBasic = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <Controller
-                name='lastName'
+                name='lastname'
                 control={control}
                 rules={{ required: true }}
                 render={({ field }) => (
@@ -89,61 +85,13 @@ const FormValidationBasic = () => {
                     {...field}
                     fullWidth
                     label='Last Name'
-                    placeholder='Doe'
+                    placeholder='Last Name'
                     {...(errors.lastName && { error: true, helperText: 'This field is required.' })}
                   />
                 )}
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Controller
-                name='email'
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    fullWidth
-                    type='email'
-                    label='Email'
-                    placeholder='johndoe@gmail.com'
-                    {...(errors.email && { error: true, helperText: 'This field is required.' })}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Controller
-                name='password'
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    fullWidth
-                    label='Password'
-                    placeholder='············'
-                    id='form-validation-basic-password'
-                    type={isPasswordShown ? 'text' : 'password'}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position='end'>
-                          <IconButton
-                            edge='end'
-                            onClick={handleClickShowPassword}
-                            onMouseDown={e => e.preventDefault()}
-                            aria-label='toggle password visibility'
-                          >
-                            <i className={isPasswordShown ? 'ri-eye-off-line' : 'ri-eye-line'} />
-                          </IconButton>
-                        </InputAdornment>
-                      )
-                    }}
-                    {...(errors.password && { error: true, helperText: 'This field is required.' })}
-                  />
-                )}
-              />
-            </Grid>
+
             <Grid item xs={12} sm={6}>
               <Controller
                 name='dob'
@@ -169,79 +117,130 @@ const FormValidationBasic = () => {
                 )}
               />
             </Grid>
+
             <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel error={Boolean(errors.select)}>Country</InputLabel>
+              <Controller
+                name='country'
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    label='Country'
+                    placeholder='Country'
+                    {...(errors.lastName && { error: true, helperText: 'This field is required.' })}
+                  />
+                )}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl error={Boolean(errors.checkbox)}>
                 <Controller
-                  name='select'
+                  name='adress_parent'
                   control={control}
                   rules={{ required: true }}
                   render={({ field }) => (
-                    <Select label='Country' {...field} error={Boolean(errors.select)}>
-                      <MenuItem value='UK'>UK</MenuItem>
-                      <MenuItem value='USA'>USA</MenuItem>
-                      <MenuItem value='Australia'>Australia</MenuItem>
-                      <MenuItem value='Germany'>Germany</MenuItem>
+                    <FormControlLabel control={<Checkbox {...field} />} label='Same as Parents address' />
+                  )}
+                />
+                {errors.checkbox && <FormHelperText error>This field is required.</FormHelperText>}
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name='street'
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    label='Street'
+                    placeholder='Street And House Number'
+                    {...(errors.lastName && { error: true, helperText: 'This field is required.' })}
+                  />
+                )}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+                <InputLabel error={Boolean(errors.select)}>Courses</InputLabel>
+                <Controller
+                  name='courses'
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Select label='Courses' {...field} error={Boolean(errors.select)}>
+                      <MenuItem value='c1'>course1</MenuItem>
+                      <MenuItem value='c2'>course2</MenuItem>
                     </Select>
                   )}
                 />
                 {errors.select && <FormHelperText error>This field is required.</FormHelperText>}
               </FormControl>
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={12} sm={6}>
               <Controller
-                name='textarea'
+                name='city'
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    label='City'
+                    placeholder='City'
+                    {...(errors.lastName && { error: true, helperText: 'This field is required.' })}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name='nationality'
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    label='Nationality'
+                    placeholder='Nationality'
+                    {...(errors.lastName && { error: true, helperText: 'This field is required.' })}
+                  />
+                )}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name='notes'
                 control={control}
                 rules={{ required: true }}
                 render={({ field }) => (
                   <TextField
                     {...field}
                     rows={4}
+                    cols={4}
                     fullWidth
                     multiline
-                    label='Bio'
+                    label='Notes'
                     {...(errors.textarea && { error: true, helperText: 'This field is required.' })}
                   />
                 )}
               />
             </Grid>
-            <Grid item xs={12}>
-              <FormControl error={Boolean(errors.radio)}>
-                <FormLabel>Gender</FormLabel>
-                <Controller
-                  name='radio'
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field }) => (
-                    <RadioGroup row {...field} name='radio-buttons-group'>
-                      <FormControlLabel value='female' control={<Radio />} label='Female' />
-                      <FormControlLabel value='male' control={<Radio />} label='Male' />
-                      <FormControlLabel value='other' control={<Radio />} label='Other' />
-                    </RadioGroup>
-                  )}
-                />
-                {errors.radio && <FormHelperText error>This field is required.</FormHelperText>}
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl error={Boolean(errors.checkbox)}>
-                <Controller
-                  name='checkbox'
-                  control={control}
-                  rules={{ required: true }}
-                  render={({ field }) => (
-                    <FormControlLabel control={<Checkbox {...field} />} label='Agree to our terms and conditions' />
-                  )}
-                />
-                {errors.checkbox && <FormHelperText error>This field is required.</FormHelperText>}
-              </FormControl>
-            </Grid>
+
             <Grid item xs={12} className='flex gap-4'>
               <Button variant='contained' type='submit'>
-                Submit
+                Save
               </Button>
               <Button variant='outlined' type='reset' onClick={() => reset()}>
-                Reset
+                Cancel
               </Button>
             </Grid>
           </Grid>
@@ -251,4 +250,4 @@ const FormValidationBasic = () => {
   )
 }
 
-export default FormValidationBasic
+export default RegisterChildren

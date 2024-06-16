59ebe054206dbda98abc350d3832c4a53f92a58e
sulaf-93 <sulaf-93>
2024-06-16 22:46:28 +0300

    API view children done

--- FekraHubAPI/Controllers/StudentController.cs
+++ FekraHubAPI/Controllers/StudentController.cs
@@ -5,8 +5,12 @@ using FekraHubAPI.EmailSender;
 using FekraHubAPI.MapModels.Courses;
 using FekraHubAPI.Repositories.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Security.Claims;
 
 namespace FekraHubAPI.Controllers
@@ -115,6 +119,31 @@ namespace FekraHubAPI.Controllers
         [HttpPost]
         //[Authorize]
         public async Task<IActionResult> InsertStudent([FromForm] Map_Student student)
+        }
+        [HttpGet("ByParent")]
+        [Authorize]
+        public async Task<IActionResult> GetStudentsByParent()//
+        {
+            var parentId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+    if (string.IsNullOrEmpty(parentId))
+    {
+        return Unauthorized("Parent ID not found in token.");
+    }
+
+    var students = (await _studentRepo.GetRelation()).Where(x => x.ParentID == parentId);
+    
+            var result = students.Select(z => new
+            {
+                student = z,
+                course = new { z.Course.Id, z.Course.Name, z.Course.Capacity, z.Course.StartDate, z.Course.EndDate, z.Course.Price }
+            }).ToList();
+            
+
+         return Ok(result);
+       }
+        [HttpPost("student/add")]
+        public async Task<IActionResult> AddStudent([FromForm] Map_Student student)
         {
             student.ParentID = "0788b331-7b8e-4294-882e-560c884b4f8f";
             if (!ModelState.IsValid)

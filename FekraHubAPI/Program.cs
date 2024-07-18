a44eb31947f26cecc98f76a848835a8a22a09ab7
basel.slaby@gmail.com <basel.slaby@gmail.com>
2024-07-18 21:54:42 +0300

    docker New

--- FekraHubAPI/Program.cs
+++ FekraHubAPI/Program.cs
@@ -52,6 +52,11 @@ builder.Services.AddCors(options =>
                .AllowCredentials();
     });
 });
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80); // HTTP
+    options.ListenAnyIP(443);
+});
 //Adding Authentication 
 builder.Services.AddAuthentication(options =>
 {
@@ -116,4 +121,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://0.0.0.0:80");

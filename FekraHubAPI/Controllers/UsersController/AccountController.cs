4ad143aeb2b6aaf12bed7fd0e4eb04bd39362651
basel.slaby@gmail.com <basel.slaby@gmail.com>
2024-08-02 11:59:51 +0300

    role permissions

--- FekraHubAPI/Controllers/UsersController/AccountController.cs
+++ FekraHubAPI/Controllers/UsersController/AccountController.cs
@@ -448,6 +448,26 @@ namespace FekraHubAPI.Controllers.UsersController
             }
         }
         [HttpPost("[action]")]
+        public async Task<IActionResult> RolePermissions()
+        {
+            var rolesWithPermissions = new Dictionary<string, List<string>>();
+
+            var roles = await _roleManager.Roles.ToListAsync();
+            foreach (var role in roles)
+            {
+                var roleClaims = await _roleManager.GetClaimsAsync(role);
+                if (!rolesWithPermissions.ContainsKey(role.Name))
+                {
+                    rolesWithPermissions[role.Name] = new List<string>();
+                }
+                foreach (var roleClaim in roleClaims)
+                {
+                    rolesWithPermissions[role.Name].Add(roleClaim.Value);
+                }
+            }
+            return Ok(rolesWithPermissions );
+        }
+        [HttpPost("[action]")]
         public async Task<IActionResult> Logout()
         {
             try

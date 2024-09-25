d7ca5e090e86754939290c10e716c6ffd69897ff
basel.slaby@gmail.com <basel.slaby@gmail.com>
2024-09-25 14:25:10 +0300

    fix mesage sender try catch

--- FekraHubAPI/Controllers/MessageSenderController.cs
+++ FekraHubAPI/Controllers/MessageSenderController.cs
@@ -21,11 +21,11 @@ namespace FekraHubAPI.Controllers
         private readonly IRepository<ApplicationUser> _UserRepo;
         private readonly IRepository<SchoolInfo> _schoolInfoRepo;
         private readonly IRepository<Student> _studentRepo;
-        private readonly ILogger<PayRollsController> _logger;
+        private readonly ILogger<MessageSenderController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         public MessageSenderController(IRepository<MessageSender> messageSenderRepo,
-            IRepository<ApplicationUser> userRepo, IRepository<SchoolInfo> schoolInfoRepo, ILogger<PayRollsController> logger,
+            IRepository<ApplicationUser> userRepo, IRepository<SchoolInfo> schoolInfoRepo, ILogger<MessageSenderController> logger,
             UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager , IRepository<Student> studentRepo)
         {
             _messageSenderRepo = messageSenderRepo;
@@ -41,14 +41,16 @@ namespace FekraHubAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> GetMessages([FromQuery] PaginationParameters paginationParameters)
         {
-            var Allmessages = await _messageSenderRepo.GetRelationAsQueryable(
-                include:x=>x.Include(z=>z.UserMessages).ThenInclude(z=>z.User),
-                selector:x=> new
+            try
+            {
+                var Allmessages = await _messageSenderRepo.GetRelationAsQueryable(
+                include: x => x.Include(z => z.UserMessages).ThenInclude(z => z.User),
+                selector: x => new
                 {
                     x.Id,
                     x.Message,
                     x.Date,
-                    User = x.UserMessages.Select(z=> new
+                    User = x.UserMessages.Select(z => new
                     {
                         z.User.Id,
                         z.User.FirstName,
@@ -57,18 +59,27 @@ namespace FekraHubAPI.Controllers
                     })
 
                 },
-                asNoTracking:true
+                asNoTracking: true
                 );
-            var messages = await _messageSenderRepo.GetPagedDataAsync(Allmessages, paginationParameters);
-            return Ok(new { messages.TotalCount, messages.PageSize, messages.TotalPages, messages.CurrentPage, Messages = messages.Data });
+                var messages = await _messageSenderRepo.GetPagedDataAsync(Allmessages, paginationParameters);
+                return Ok(new { messages.TotalCount, messages.PageSize, messages.TotalPages, messages.CurrentPage, Messages = messages.Data });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(HandleLogFile.handleErrLogFile(User, "MessageSenderController", ex.Message));
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [Authorize(Policy = "MessageSender")]
         [HttpGet("UserMessages")]
         public async Task<IActionResult> GetUserMessages(string UserId)
         {
-            var messages = await _messageSenderRepo.GetRelationList(
-                where:x=>x.UserMessages.Select(z=>z.UserId).Contains(UserId),
+            try
+            {
+                var messages = await _messageSenderRepo.GetRelationList(
+                where: x => x.UserMessages.Select(z => z.UserId).Contains(UserId),
                 selector: x => new
                 {
                     x.Id,
@@ -77,7 +88,14 @@ namespace FekraHubAPI.Controllers
                 },
                 asNoTracking: true
                 );
-            return Ok(messages);
+                return Ok(messages);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(HandleLogFile.handleErrLogFile(User, "MessageSenderController", ex.Message));
+                return BadRequest(ex.Message);
+            }
+            
         }
         public class MessagDTO
         {
@@ -94,168 +112,181 @@ namespace FekraHubAPI.Controllers
         [HttpPost("UserMessages")]
         public async Task<IActionResult> GetUserMessages([FromForm] MessagDTO messagDTO)
         {
-            if(!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
-            }
-            if((messagDTO.UserId == null || !messagDTO.UserId.Any()) && messagDTO.Role == null && messagDTO .CourseId == null)
-            {
-                return BadRequest("There are no users to send notifications to.");
-            }
-            List<string> Emails = new List<string>();
-            List<ApplicationUser> allUsers = new List<ApplicationUser>();
-            if(messagDTO.UserId != null && messagDTO.UserId.Any())
-            {
-                var users = await _UserRepo.GetRelationList(
-                where: x => messagDTO.UserId.Contains(x.Id),
-                include: x => x.Include(u => u.UserMessages),
-                selector: x => x
-                );
-                if (users == null || !users.Any())
+                if (!ModelState.IsValid)
                 {
-                    return BadRequest("User not found");
+                    return BadRequest(ModelState);
                 }
-                else
+                if ((messagDTO.UserId == null || !messagDTO.UserId.Any()) && messagDTO.Role == null && messagDTO.CourseId == null)
                 {
-                    allUsers.AddRange(users);
-                    users.ForEach(x => Emails.Add(x.Email));
+                    return BadRequest("There are no users to send notifications to.");
                 }
-                
-            }
-            if(messagDTO.Role != null)
-            {
-                var role = await _roleManager.FindByNameAsync(messagDTO.Role);
-                if (role == null)
+                List<string> Emails = new List<string>();
+                List<ApplicationUser> allUsers = new List<ApplicationUser>();
+                if (messagDTO.UserId != null && messagDTO.UserId.Any())
                 {
-                    return NotFound(new { Message = "Role not found" });
-                }
-
-                var usersInRole = (await _userManager.GetUsersInRoleAsync(messagDTO.Role)).ToList();
-                if (usersInRole == null || !usersInRole.Any())
-                {
-                    return BadRequest("User not found");
-                }
-                else
-                {
-                    allUsers.AddRange(usersInRole);
-                    usersInRole.ForEach(x => Emails.Add(x.Email));
-                }
-            }
-            if (messagDTO.CourseId != null)
-            {
-                var Parents = await _studentRepo.GetRelationList(
-                    where:x=>x.CourseID == messagDTO.CourseId,
-                    include:x=>x.Include(z=>z.User),
-                    selector:x=>x.User,
-                    asNoTracking:true
+                    var users = await _UserRepo.GetRelationList(
+                    where: x => messagDTO.UserId.Contains(x.Id),
+                    include: x => x.Include(u => u.UserMessages),
+                    selector: x => x
                     );
-                if (Parents == null || !Parents.Any())
-                {
-                    return BadRequest("User not found");
-                }
-                else
-                {
-                    allUsers.AddRange(Parents);
-                    Parents.ForEach(x => Emails.Add(x.Email));
-                }
-            }
-
-            HashSet<string> uniqueEmails = new HashSet<string>(Emails);
-            Emails = uniqueEmails.ToList();
-            List<ApplicationUser> uniqueUsers = allUsers
-            .GroupBy(user => user.Email)
-            .Select(group => group.First())
-            .ToList();
-
-            var schoolInfo = await _schoolInfoRepo.GetRelationSingle(
-                selector: x => new
-                {
-                    x.SchoolName, x.EmailServer, x.EmailPortNumber, x.FromEmail, x.Password
-                },
-                asNoTracking: true,
-                returnType: QueryReturnType.Single
-                );
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress(schoolInfo.SchoolName, schoolInfo.FromEmail));
-            foreach (var Email in Emails)
-            {
-                message.Bcc.Add(new MailboxAddress("", Email));
-            }
-            if (messagDTO.ExternalEmails != null && messagDTO.ExternalEmails.Any())
-            {
-                foreach(var email in messagDTO.ExternalEmails)
-                {
-                    message.Bcc.Add(new MailboxAddress("", email));
-                }
-            }
-
-            message.Subject = messagDTO.Subject;
-
-            var bodyBuilder = new BodyBuilder
-            {
-                
-                HtmlBody = messagDTO.Message
-            };
-
-            if (messagDTO.Files != null && messagDTO.Files.Any())
-            {
-                foreach (var file in messagDTO.Files)
-                {
-                    byte[] fileBytes;
-                    using (var ms = new MemoryStream())
+                    if (users == null || !users.Any())
                     {
-                        await file.CopyToAsync(ms);
-                        fileBytes = ms.ToArray();
+                        return BadRequest("User not found");
                     }
-                    var contentType = new ContentType("application",file.ContentType);
-                    bodyBuilder.Attachments.Add(file.FileName, fileBytes, contentType);
+                    else
+                    {
+                        allUsers.AddRange(users);
+                        users.ForEach(x => Emails.Add(x.Email));
+                    }
+
                 }
+                if (messagDTO.Role != null)
+                {
+                    var role = await _roleManager.FindByNameAsync(messagDTO.Role);
+                    if (role == null)
+                    {
+                        return NotFound(new { Message = "Role not found" });
+                    }
+
+                    var usersInRole = (await _userManager.GetUsersInRoleAsync(messagDTO.Role)).ToList();
+                    if (usersInRole == null || !usersInRole.Any())
+                    {
+                        return BadRequest("User not found");
+                    }
+                    else
+                    {
+                        allUsers.AddRange(usersInRole);
+                        usersInRole.ForEach(x => Emails.Add(x.Email));
+                    }
+                }
+                if (messagDTO.CourseId != null)
+                {
+                    var Parents = await _studentRepo.GetRelationList(
+                        where: x => x.CourseID == messagDTO.CourseId,
+                        include: x => x.Include(z => z.User),
+                        selector: x => x.User,
+                        asNoTracking: true
+                        );
+                    if (Parents == null || !Parents.Any())
+                    {
+                        return BadRequest("User not found");
+                    }
+                    else
+                    {
+                        allUsers.AddRange(Parents);
+                        Parents.ForEach(x => Emails.Add(x.Email));
+                    }
+                }
+
+                HashSet<string> uniqueEmails = new HashSet<string>(Emails);
+                Emails = uniqueEmails.ToList();
+                List<ApplicationUser> uniqueUsers = allUsers
+                .GroupBy(user => user.Email)
+                .Select(group => group.First())
+                .ToList();
+
+                var schoolInfo = await _schoolInfoRepo.GetRelationSingle(
+                    selector: x => new
+                    {
+                        x.SchoolName,
+                        x.EmailServer,
+                        x.EmailPortNumber,
+                        x.FromEmail,
+                        x.Password
+                    },
+                    asNoTracking: true,
+                    returnType: QueryReturnType.Single
+                    );
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(schoolInfo.SchoolName, schoolInfo.FromEmail));
+                foreach (var Email in Emails)
+                {
+                    message.Bcc.Add(new MailboxAddress("", Email));
+                }
+                if (messagDTO.ExternalEmails != null && messagDTO.ExternalEmails.Any())
+                {
+                    foreach (var email in messagDTO.ExternalEmails)
+                    {
+                        message.Bcc.Add(new MailboxAddress("", email));
+                    }
+                }
+
+                message.Subject = messagDTO.Subject;
+
+                var bodyBuilder = new BodyBuilder
+                {
+
+                    HtmlBody = messagDTO.Message
+                };
+
+                if (messagDTO.Files != null && messagDTO.Files.Any())
+                {
+                    foreach (var file in messagDTO.Files)
+                    {
+                        byte[] fileBytes;
+                        using (var ms = new MemoryStream())
+                        {
+                            await file.CopyToAsync(ms);
+                            fileBytes = ms.ToArray();
+                        }
+                        var contentType = new ContentType("application", file.ContentType);
+                        bodyBuilder.Attachments.Add(file.FileName, fileBytes, contentType);
+                    }
+                }
+
+                message.Body = bodyBuilder.ToMessageBody();
+
+                using (var client = new SmtpClient())
+                {
+                    try
+                    {
+                        await client.ConnectAsync(schoolInfo.EmailServer, schoolInfo.EmailPortNumber, MailKit.Security.SecureSocketOptions.Auto);
+                        await client.AuthenticateAsync(schoolInfo.FromEmail, schoolInfo.Password);
+                        await client.SendAsync(message);
+                        await client.DisconnectAsync(true);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(HandleLogFile.handleErrLogFile(User, "MessageSenderController", ex.Message));
+                        return BadRequest(ex.Message);
+                    }
+
+                }
+
+
+                var messageSender = new MessageSender
+                {
+                    Message = messagDTO.Message,
+                    UserMessages = uniqueUsers.Select(user => new UserMessage
+                    {
+                        UserId = user.Id
+                    }).ToList()
+                };
+                await _messageSenderRepo.Add(messageSender);
+
+
+                return Ok(new
+                {
+                    messageSender.Id,
+                    messageSender.Date,
+                    messageSender.Message,
+                    User = uniqueUsers.Select(x => new
+                    {
+                        x.Id,
+                        x.FirstName,
+                        x.LastName,
+                        x.Email
+                    })
+                });
             }
-
-            message.Body = bodyBuilder.ToMessageBody();
-
-            using (var client = new SmtpClient())
+            catch (Exception ex)
             {
-                try
-                {
-                    await client.ConnectAsync(schoolInfo.EmailServer, schoolInfo.EmailPortNumber, MailKit.Security.SecureSocketOptions.Auto);
-                    await client.AuthenticateAsync(schoolInfo.FromEmail, schoolInfo.Password);
-                    await client.SendAsync(message);
-                    await client.DisconnectAsync(true);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(HandleLogFile.handleErrLogFile(User, "MessageSenderController", ex.Message));
-                    return BadRequest(ex.Message);
-                }
-
+                _logger.LogError(HandleLogFile.handleErrLogFile(User, "MessageSenderController", ex.Message));
+                return BadRequest(ex.Message);
             }
-
-
-            var messageSender = new MessageSender
-            {
-                Message = messagDTO.Message,
-                UserMessages = uniqueUsers.Select(user => new UserMessage
-                {
-                    UserId = user.Id
-                }).ToList()
-            };
-            await _messageSenderRepo.Add(messageSender);
-
-
-            return Ok(new
-            {
-                messageSender.Id,
-                messageSender.Date,
-                messageSender.Message,
-                User = uniqueUsers.Select(x=>new
-                {
-                    x.Id,
-                    x.FirstName,
-                    x.LastName,
-                    x.Email
-                })
-            });
+           
         }
 
 

74b2c1262181527939c421c85fba0ac69e4922b4
basel.slaby@gmail.com <basel.slaby@gmail.com>
2024-11-05 17:47:35 +0200

    end test

--- FekraHubAPI/Program.cs
+++ FekraHubAPI/Program.cs
@@ -150,11 +150,7 @@ builder.Services.Configure<FormOptions>(options =>
 
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate(); // يقوم بتطبيق أي هجرات جديدة
-}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 

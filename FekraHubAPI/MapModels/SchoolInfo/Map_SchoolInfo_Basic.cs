474f2e1f2956b76b96203d77577de40d6b765a13
basel <basel@BASEL>
2025-11-14 16:15:35 +0200

    school infos

--- FekraHubAPI/MapModels/SchoolInfo/Map_SchoolInfo_Basic.cs
+++ FekraHubAPI/MapModels/SchoolInfo/Map_SchoolInfo_Basic.cs
@@ -1,24 +1,19 @@
-﻿using AutoMapper;
-using FekraHubAPI.Data.Models;
-using FekraHubAPI.MapModels.Courses;
-using System.ComponentModel.DataAnnotations;
+﻿
 
 namespace FekraHubAPI.MapModels.SchoolInfo
 {
     public class Map_SchoolInfo_Basic 
     {
         
-        public string SchoolName { get; set; }
-        public string SchoolOwner { get; set; }
-        public IFormFile Logo { get; set; }
-        
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        
-        
+        public string? SchoolName { get; set; }
+        public string? SchoolOwner { get; set; }
+        public IFormFile? Logo { get; set; }
+        public string? FacebookLink { get; set; }
+        public string? InstagramLink { get; set; }
+        public string? PrivacyPolicyLink { get; set; }
 
-       
+
+
+
     }
 }
--- FekraHubAPI/MapModels/SchoolInfo/Map_schoolInfo_EmailSender.cs
+++ FekraHubAPI/MapModels/SchoolInfo/Map_schoolInfo_EmailSender.cs
@@ -2,9 +2,9 @@
 {
     public class Map_schoolInfo_EmailSender
     {
-        public string EmailServer { get; set; }
-        public int EmailPortNumber { get; set; }
-        public string FromEmail { get; set; }
-        public string Password { get; set; }
+        public string? EmailServer { get; set; }
+        public int? EmailPortNumber { get; set; }
+        public string? FromEmail { get; set; }
+        public string? Password { get; set; }
     }
 }
